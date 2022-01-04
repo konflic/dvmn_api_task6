63a4dc229a56c34f475a556e4c84dd8864c1f746
konflic <konflic@mail.ru>
2022-01-04 20:51:46 +0300

    Complete the script

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import requests
+import random
 
 from urllib.parse import urlparse, urljoin
 from dotenv import load_dotenv
@@ -12,6 +13,7 @@ load_dotenv()
 
 CLIENT_ID = os.getenv("CLIENT_ID")
 ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+GROUP_ID = os.getenv("GROUP_ID")
 
 
 def get_comic(number):
@@ -20,6 +22,15 @@ def get_comic(number):
     )
     response.raise_for_status()
     return response.json()
+
+
+def get_random_comic():
+    response = requests.get(
+        url="https://xkcd.com/info.0.json"
+    )
+    response.raise_for_status()
+    last_comic = int(response.json()["num"])
+    return get_comic(random.randint(1, last_comic))
 
 
 def download_picture(picture_url):
@@ -37,25 +48,34 @@ def download_picture(picture_url):
 def get_upload_url():
     response = requests.get(
         url=f"{API_URL}/method/photos.getWallUploadServer",
-        params={"access_token": ACCESS_TOKEN, "v": API_VERSION},
+        params={
+            "access_token": ACCESS_TOKEN,
+             "v": API_VERSION,
+             "group_id": GROUP_ID
+         },
     )
     response.raise_for_status()
     return response.json()["response"]["upload_url"]
 
 
-def upload_picture(file_name):
-    upload_url = get_upload_url()
-    with open(f"Files/{file_name}", "rb") as file:
+def upload_picture(file_name, upload_url):
+    file_path = f"Files/{file_name}"
+    with open(file_path, "rb") as file:
         files = {"file1": file}
         response = requests.post(upload_url, files=files)
         response.raise_for_status()
+    os.remove(file_path)
     return response.json()
 
 
 def save_picture(upload_data):
     response = requests.post(
         url=f"{API_URL}/method/photos.saveWallPhoto",
-        params={"access_token": ACCESS_TOKEN, "v": API_VERSION},
+        params={
+            "access_token": ACCESS_TOKEN, 
+            "v": API_VERSION,
+            "group_id": GROUP_ID
+        },
         data={
             "server": upload_data["server"],
             "hash": upload_data["hash"],
@@ -63,27 +83,37 @@ def save_picture(upload_data):
         },
     )
     response.raise_for_status()
-    return response.json()
+    return response.json()["response"][0]
 
 
-def publish_picture_on_wall(save_data):
+def publish_picture_on_wall(save_data, message):
+    attachment = f"photo{save_data['owner_id']}_{save_data['id']}"
     response = requests.post(
         url=f"{API_URL}/method/wall.post",
         params={
             "access_token": ACCESS_TOKEN,
             "v": API_VERSION,
         },
+        data={
+            "owner_id": "-" + GROUP_ID,
+            "from_group": 1,
+            "attachments": attachment,
+            "message": message,
+        },
     )
     response.raise_for_status()
+    return response.json()
 
 
 if __name__ == "__main__":
-    comic_data = get_comic(600)
+    comic_data = get_random_comic()
     comic_img = comic_data["img"]
     comic_title = comic_data["alt"]
 
     file_name = download_picture(comic_img)
-    upload_data = upload_picture(file_name)
+    upload_url = get_upload_url()
+
+    upload_data = upload_picture(file_name, upload_url)
     save_data = save_picture(upload_data)
 
-    print(save_data)
+    publish_picture_on_wall(save_data, message=comic_title)
