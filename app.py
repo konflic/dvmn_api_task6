aaf9935c3cc4dd062aa8624bd8625f384a1bad54
konflic <konflic@mail.ru>
2022-01-04 18:04:21 +0300

    Added get_comic function

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import os
+import requests
+
 from dotenv import load_dotenv
 
 API_VERSION = "5.131"
@@ -7,3 +9,13 @@ load_dotenv()
 
 client_id = os.getenv("CLIENT_ID")
 access_token = os.getenv("ACCESS_TOKEN")
+
+def get_comic(number):
+    response = requests.get(
+        url=f"https://xkcd.com/{number}/info.0.json",
+    )
+    response.raise_for_status()
+    return response.json()
+
+if __name__ == "__main__":
+    print(get_comic(614))
